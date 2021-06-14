7ed1014fde33a82706e0022657acf3930438e104
Haways <Haways@langleysys@gmail.com>
2021-06-14 03:38:42 -0300

    refac

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,6 +3,6 @@
 void _start()
 {
     mv_cursor(position_xy(12, 3));
-    write_string(12,"astronaut_8 space tester");
-    loop();
+    write_string(12, "astronaut_39 color tester ");
+    write_string(10, "astronaut_40 color tester ");
 }
--- src/kernel/std.c
+++ src/kernel/std.c
@@ -2,19 +2,19 @@
 #include "typedef.c"
 #include "io.c"
 
-# define vga_mem (uint_16 *)0xb8000
+# define vga_mem (uint_8 *)0xb8000
 # define vga_width 80
 
-uint_16 cursorpos = 0;
+uint_16 cursorpos;
 
 void loop(){while(1){}}
 
 void mv_cursor(uint_16 position)
 {
     outbyte(0x3d4, 0x0f);
-    outbyte(0x3d5, (uint_8)(position & 0xff));
+    outbyte(0x3d5, (uint_8)(cursorpos & 0xff));
     outbyte(0x3d4, 0x0e);
-    outbyte(0x3d5, (uint_8)((position >> 8) & 0xff));
+    outbyte(0x3d5, (uint_8)((cursorpos >> 8) & 0xff));
     cursorpos = position;
 }
 
@@ -25,13 +25,11 @@ uint_16 position_xy(uint_8 x, uint_8 y)
 
 void write_string(int colour, const char *string)
 {
-    uint_16 position = cursorpos;
-    uint_8 *video = (uint_8*)(0xB8000)+position*2;
+    uint_16 position = cursorpos; 
     while( *string != 0 )
     {
-        *video++ = *string++;
-        *video++ = colour;
-        position++;
+        *( vga_mem + position*2 ) = *string++;
+        *( vga_mem + position*2 + 1) = colour;
+        mv_cursor(position++);
     }
-    mv_cursor(position);
 }
